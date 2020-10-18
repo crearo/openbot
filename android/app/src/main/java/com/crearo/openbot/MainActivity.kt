d524a7641d7fb1ab9d53ee653cd140cd50b29224
rish <bhardwaj.rish@gmail.com>
2020-10-18 12:08:16 +0200

    Migrate to AndroidX

--- android/app/src/main/java/com/crearo/openbot/MainActivity.kt
+++ android/app/src/main/java/com/crearo/openbot/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.crearo.openbot
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
