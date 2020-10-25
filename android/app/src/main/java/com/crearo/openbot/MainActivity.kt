aefbfbe84de988166d3b05d032858702216e854b
rish <bhardwaj.rish@gmail.com>
2020-10-25 15:59:42 +0100

    Add bluetooth connection to controller
    Argh why this not working buh

--- android/app/src/main/java/com/crearo/openbot/MainActivity.kt
+++ android/app/src/main/java/com/crearo/openbot/MainActivity.kt
@@ -1,5 +1,9 @@
 package com.crearo.openbot
 
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothDevice.ACTION_ACL_CONNECTED
+import android.bluetooth.BluetoothDevice.ACTION_ACL_DISCONNECTED
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
@@ -7,30 +11,49 @@ import android.content.IntentFilter
 import android.os.Bundle
 import android.view.KeyEvent
 import android.view.MotionEvent
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.crearo.openbot.controller.ControlEventForCar
 import com.crearo.openbot.controller.Dpad
 import com.crearo.openbot.controller.DpadState
+import java.io.IOException
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        private const val TAG = "MainActivity"
+    }
+
     private val dpad = Dpad()
     private lateinit var tvInfo: TextView
-    private lateinit var tvConnected: TextView
+    private lateinit var tvUsbConnected: TextView
+    private lateinit var tvControllerConnected: TextView
     private val dpadState = DpadState(0f, 0f, 0f, 0f)
     private val controlEventForCar = ControlEventForCar(0f, 0f)
     private lateinit var usbConnection: UsbConnection
     private lateinit var localBroadcastManager: LocalBroadcastManager
 
+    private lateinit var bluetoothAdapter: BluetoothAdapter
+    private val bluetoothBroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            val device = intent?.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE) as BluetoothDevice
+            tvControllerConnected.text = "Controller Status: $device"
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         tvInfo = findViewById(R.id.tv_info)
-        tvConnected = findViewById(R.id.tv_connected)
+        tvUsbConnected = findViewById(R.id.tv_usb_connected)
+        tvControllerConnected = findViewById(R.id.tv_controller_connected)
         usbConnection = UsbConnection(applicationContext)
         localBroadcastManager = LocalBroadcastManager.getInstance(this)
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
 
         val intentFilter = IntentFilter()
         intentFilter.addAction("connection")
@@ -39,20 +62,27 @@ class MainActivity : AppCompatActivity() {
             override fun onReceive(context: Context?, intent: Intent?) {
                 runOnUiThread {
                     when (intent?.action) {
-                        "connection" -> tvConnected.text = intent.getStringExtra("action")
+                        "connection" -> tvUsbConnected.text = intent.getStringExtra("action")
                     }
                 }
             }
         }, intentFilter)
+
+        findViewById<Button>(R.id.btn_pair).setOnClickListener { bondController() }
     }
 
     override fun onResume() {
         super.onResume()
+        val bluetoothConnectionIntentFilter = IntentFilter()
+        bluetoothConnectionIntentFilter.addAction(ACTION_ACL_CONNECTED)
+        bluetoothConnectionIntentFilter.addAction(ACTION_ACL_DISCONNECTED)
+        registerReceiver(bluetoothBroadcastReceiver, bluetoothConnectionIntentFilter)
         usbConnection.startListener()
     }
 
     override fun onPause() {
         super.onPause()
+        unregisterReceiver(bluetoothBroadcastReceiver)
         usbConnection.stopListener()
         usbConnection.stopUsbConnection()
     }
@@ -94,11 +124,49 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun onDpadEvent(dpadState: DpadState) {
-        // textView.text = "$dpadState\n${textView.text}"
         controlEventForCar.onDpadEvent(dpadState)
         tvInfo.text = "$controlEventForCar"
         if (!usbConnection.isBusy()) {
             usbConnection.send("${controlEventForCar.left},${controlEventForCar.right}\n")
+        }
+    }
+
+    private fun bondController() {
+        val bluetoothDevice = getPairedProController().also { tvControllerConnected.text = "Found Device: ${it.isPresent}" }
+        if (bluetoothDevice.isPresent) {
+            connectBluetoothDevice(bluetoothDevice.get())
+        }
+    }
+
+    private fun getPairedProController(): Optional<BluetoothDevice> {
+        // 59:C7:BE:FB:00:A2
+        return Optional.ofNullable(bluetoothAdapter.getRemoteDevice("59:C7:BE:FB:00:A2"))
+//        val pairedDevices: Set<BluetoothDevice> = bluetoothAdapter.bondedDevices
+//        return pairedDevices.stream().filter { device: BluetoothDevice -> device.name.contains("Pro Controller") }.findFirst()
+    }
+
+    private fun connectBluetoothDevice(bluetoothDevice: BluetoothDevice) {
+        /*        val gatt = bluetoothDevice.connectGatt(this, true, object : BluetoothGattCallback() {
+            override fun onConnectionStateChange(gatt: BluetoothGatt?, status: Int, newState: Int) {
+                super.onConnectionStateChange(gatt, status, newState)
+                Log.d(TAG, "ConnectionState: $status $newState")
+            }
+        })
+
+        gatt.connect()*/
+
+        val socket = bluetoothDevice.createRfcommSocketToServiceRecord(UUID.randomUUID())
+        if (bluetoothAdapter.isDiscovering) {
+            tvControllerConnected.text = "Discovering right now, try again later"
+            return
+        }
+        if (!socket.isConnected) {
+            tvControllerConnected.text = "Attempting connection"
+            try {
+                socket.connect()
+            } catch (e: IOException) {
+                tvControllerConnected.text = "Failed to connect ${e.message}"
+            }
         }
     }
 }
